3c0a3f25a16a68cb3d1e8d0f3d4ec2faf260d9e1
BarelHeby <barelheby03@gmail.com>
2024-08-12 12:11:50 +0300

    chore: Update gunicorn.conf.py to remove unused worker_class, keepalive, errorlog, and accesslog options and adjust timeout value

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -2,8 +2,4 @@ import multiprocessing
 
 bind = "0.0.0.0:80"
 workers = multiprocessing.cpu_count() * 2 + 1
-worker_class = "sync"
 timeout = 120
-keepalive = 2
-errorlog = "-"  # Log errors to stderr
-accesslog = "-"  # Log access to stdout
